7b6a2f02651b639b8c95706c86211a4509c648e2
Hasan Sabri <hsabri2007@gmail.com>
2026-03-14 23:23:18 +0000

    heinous crimes for the sake of compatibility

--- index.ts
+++ index.ts
@@ -1,12 +1,27 @@
 
 type RouteHandler = (req: Bun.BunRequest) => Response
+type AsyncRouteHandler = (req: Bun.BunRequest) => Promise<Response>
+
+type RouteMethods = {
+	GET?: FlatRouteTypes,
+	HEAD?: FlatRouteTypes,
+	POST?: FlatRouteTypes,
+	PUT?: FlatRouteTypes,
+	DELETE?: FlatRouteTypes,
+	CONNECT?: FlatRouteTypes,
+	OPTIONS?: FlatRouteTypes,
+	TRACE?: FlatRouteTypes,
+	PATCH?: FlatRouteTypes,
+}
+
+type FlatRouteTypes = RouteHandler | AsyncRouteHandler | RouteMethods | Response | Bun.HTMLBundle
 
 type FlatRoutes = {
-	[key: string]: RouteHandler
+	[key: string]: FlatRouteTypes
 }
 
 type ModularRoutes = {
-	[key: string]: ModularRoutes | RouteHandler | Bun.HTMLBundle
+	[key: string]: ModularRoutes | FlatRouteTypes
 }
 
 function ModularRouter(routesIn: ModularRoutes): FlatRoutes
@@ -16,11 +31,24 @@ function ModularRouter(routesIn: ModularRoutes): FlatRoutes
 	{
 		for (const [key, val] of Object.entries(routes))
 		{
-
-			if (typeof val === "function" || val.index)
-				routesOut[prefix + key] = val as any
+			if (typeof val === "function" || // routehandler, asyncroutehandler
+				'index' in val || // bun.htmlbundle
+				'onmessage' in val || // response
+				'GET' in val ||
+				'HEAD' in val ||
+				'POST' in val ||
+				'PUT' in val ||
+				'DELETE' in val ||
+				'CONNECT' in val ||
+				'OPTIONS' in val ||
+				'TRACE' in val ||
+				'PATCH' in val
+			)
+				routesOut[prefix + key] = val as RouteHandler | AsyncRouteHandler | Response | Bun.HTMLBundle
 			else
+			{
 				collapse(val as ModularRoutes, prefix + key)
+			}
 		}
 	}
 	collapse(routesIn, "")
